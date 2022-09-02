9f8ac50bdef266f3d025192a38dbb21371a9ad02
Lucas Duarte <lucs.rkt@gmail.com>
2022-09-01 22:22:32 -0300

    feat(migrate/students): add fields

--- db/migrate/20220901185657_create_students.rb
+++ db/migrate/20220901185657_create_students.rb
@@ -2,6 +2,11 @@ class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
       t.string :name
+      t.string :cpf
+      t.string :gender
+      t.string :payment_way
+      t.date :birthday
+      t.integer :cellphone
 
       t.timestamps
     end

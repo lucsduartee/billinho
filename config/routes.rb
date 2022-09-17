13f272e8b99dadd9e9c2be084767fd145d5b6b3e
Lucas Duarte <lucs.rkt@gmail.com>
2022-09-16 21:57:29 -0300

    feat: create routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace 'api' do
+    namespace 'v1' do
+      resources :institutions
+      resources :bills, 
+      resources :students
+      resources :enrollments
+    end
+  end
 end
